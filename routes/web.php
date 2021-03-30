d3dc7e6a9e6dc471d8de3f9cbc5cbfb6acbabe5a
Jordan <jordanolson1@gmail.com>
2021-03-30 16:10:17 -0600

    added article model

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ Route::get('/',function(){
     return view('welcome');
 });
 Route::get('/about',function(){
-    return view('about');
+    
+    return view('about',[
+        'articles' => $article=App\Article::latest()->get()
+    ]);
 });
 Route::get('/contact',function(){
     return view('contact');
