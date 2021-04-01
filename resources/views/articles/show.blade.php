1ff98c6a41423ac6dfa817afa49ac13b792c90f6
Jordan <jordanolson1@gmail.com>
2021-04-01 17:01:06 -0600

    validation

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,14 +1,15 @@
 @extends('layout')
 
 @section('content')
-div id="wrapper">
+<div id="wrapper">
 <div id="page" class="container">
 <div id="content">
 	<div class="title">
 		
 		<span class="byline">{{$article->title}}</span> </div>
+		<p>{{$article->body}}</p>
 	<p><img src="/css/images/banner.jpg" alt="" class="image image-full" /> </p>
-	<p>{{$article->body}}</p>
+	<p>{{$article->excerpt}}</p>
 </div>
 
 @endsection
