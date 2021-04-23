8d5115e4366dd7bf4cbdd17bcfccf2656cb03ac7
Jordan <jordanolson1@gmail.com>
2021-04-23 15:57:08 -0600

    added article relationships and user relationship, database factory practice and created some Database relationships in my tags and articles tables (create_tags_table is a pivot table)
    yeet

--- app/Article.php
+++ app/Article.php
@@ -8,4 +8,19 @@ class Article extends Model
 {
     //
     protected $guarded = [];
+
+    public function path()
+    {
+        return route('articles.show', $this);
+    }
+    public function author()
+    {
+        /*, 'user_id'set foreign key as 2nd argument because the key it looks for is based on the methodName_id, so have to specify what ID we want by passing it in*/
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
