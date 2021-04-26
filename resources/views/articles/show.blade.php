c1feb2f4ff993fceaca8c41572602b510350cd81
Jordan <jordanolson1@gmail.com>
2021-04-26 16:01:58 -0600

    fixed tag naming convention

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -2,14 +2,21 @@
 
 @section('content')
 <div id="wrapper">
-<div id="page" class="container">
-<div id="content">
-	<div class="title">
-		
-		<span class="byline">{{$article->title}}</span> </div>
-		<p>{{$article->body}}</p>
-	<p><img src="/css/images/banner.jpg" alt="" class="image image-full" /> </p>
-	<p>{{$article->excerpt}}</p>
+	<div id="page" class="container">
+		<div id="content">
+			<div class="title">
+				<h2 class="byline">{{$article->title}}</h2> 
+			</div>
+			<p>{{$article->body}}</p>
+			<p><img src="/css/images/banner.jpg" alt="" class="image image-full" /> </p>
+			<p>{{$article->excerpt}}</p>
+			{!! $article->body !!}
+			<p>
+				@foreach ($article->Tags as $tag)
+					<a href="#">{{$tag->name}}</a>		
+				@endforeach
+			</p>
+		</div>
+	</div>
 </div>
-
 @endsection
